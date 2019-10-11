6a0e57fbe447765a044d457da9d2e41eed413e19
cjwales <cjwales@gmail.com>
2019-10-11 18:45:01 +0100

    added form file and template

---
+++ src/components/FilmForm.vue
@@ -0,0 +1,14 @@
+<template lang="html">
+  <form v-on:submit.prevent>
+    
+  </form>
+</template>
+
+<script>
+export default {
+  name: "film-form"
+}
+</script>
+
+<style lang="css" scoped>
+</style>
